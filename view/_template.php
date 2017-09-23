d824f44a0989b3e316bde79fb6288b5188f5c38b
everchanger <joakim.rosenstam@gmail.com>
2017-09-23 17:38:34 +0200

    fixes and tweaks
    Added granny helper as a project :)

--- view/_template.php
+++ view/_template.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Joakim Rosenstam</title>
+		<title>Joakim Rosenstam - Portfolio</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<!-- CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -54,12 +54,12 @@
 		</nav>
 				
 		<div class="profile">
-			<div class="container">
+			<div class="container container-header">
 			<div class="col-xs-12 profile-content">
 					<img class="img-profile img-circle" src="/images/joakim.jpg" alt="Joakim Rosenstam - Software Developer" /> 
 					<div class="profile-info">
 							<h1 class="profile-name">Joakim Rosenstam</h1>
-							<p class="profile-occupation">Software Developer</p>
+							<h1 class="profile-occupation">Software Developer</h1>
 					</div>
 			</div>
 			</div>
