00c1a4cb02a39724696fbb2b68329c0695200d5e
Everchanger <joakim.rosenstam@gmail.com>
2019-07-26 18:59:36 +0200

    Trying to reduce boilerplate html

--- copy-files.js
+++ copy-files.js
@@ -1,9 +1,50 @@
-var fs = require("fs-extra");
+const path = require('path');
+const fs = require('fs');
 
-fs.copy('src/js', 'public/js', function (err) {
-   if (err) return console.error(err)
+const includes = [];
+const includePath = path.join(__dirname, 'src/includes');
+const directoryPath = path.join(__dirname, 'src/html');
+const publicPath = path.join(__dirname, 'public');
+
+const includeFiles = fs.readdirSync(includePath);
+includeFiles.forEach(function (file) {
+   let filename = file;
+   const pos = filename.lastIndexOf('.html');
+   if(pos == -1) {
+      return console.log('.html extension not found on include file..');
+   }
+   filename = filename.substr(0, pos);
+   includes[filename] = fs.readFileSync(path.join(includePath, file), 'utf8');
 });
 
-fs.copy('src/html', 'public', function (err) {
-   if (err) return console.error(err)
+fs.readdir(directoryPath, function (err, files) {
+   if (err) {
+      return console.log('Unable to scan src/html directory: ' + err);
+   } 
+
+   files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      fs.readFile(path.join(directoryPath, file), 'utf8', function (err,data) {
+         if (err) {
+            return console.log(err);
+         }
+
+         let newData = data;
+         const tags = [];
+         const expression = /<include-(.*) \/>/g;
+         while ((resultArray = expression.exec(data)) !== null) {
+            const tag = resultArray[0];
+            const includeFile = resultArray[1];
+
+            if(includes[includeFile] == undefined) {
+               return console.log('Trying to use an include file that is not defined: ' + includeFile);
+            }
+            newData = newData.replace(tag, includes[includeFile]);
+         }
+
+         fs.writeFile(path.join(publicPath, file), newData, 'utf8', function (err) {
+            if (err) return console.log(err);
+         });
+      });
+   });
 });
--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const tailwindcss = require("tailwindcss");
 
 // only needed if you want to purge
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/html/*.html", "./src/css/style.css"],
+  content: ["./src/html/*.html", "./src/includes/*.html", "./src/css/style.css"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
