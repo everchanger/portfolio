8ed9504c72259760c7ceda7129d5614ea948a07d
Joakim Rosenstam <joakim.rosenstam@gmail.com>
2022-11-22 18:10:30 +0100

    O R A N G E

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,10 +1,10 @@
 <script>
-  export let header
-  export let content
-  export let href
+  export let header;
+  export let content;
+  export let href;
 </script>
 
-<a class={'link h-12 flex flex-col ' + $$props.class} href={ href }>
-  <p class="text-sm font-medium">{ header }</p>
-  <p class="">{ content }</p>
+<a class={"link h-12 flex flex-col " + $$props.class} {href}>
+  <p class="font-medium text-orange-700 text-normal">{header}</p>
+  <p class="">{content}</p>
 </a>
